a4c4b6e602ec0abc07150e16e75155b8e91ad724
Tomas McGuinness <tomas@chewedpencil.net>
2013-05-08 14:12:10 +0100

    fixed multiple registration

--- PostRoom.Management/ResidentManager.cs
+++ PostRoom.Management/ResidentManager.cs
@@ -7,48 +7,60 @@ using System.Threading.Tasks;
 
 namespace PostRoom.Management
 {
-    public class ResidentManager
+  public class ResidentManager
+  {
+    private PostRoomDataContext context;
+    private PostManager postManager;
+
+    public ResidentManager()
     {
-        private PostRoomDataContext context;
-        private PostManager postManager;
-
-        public ResidentManager()
-        {
-            context = new PostRoomDataContext();
-            postManager = new PostManager();
-        }
-
-        public void AddResident(string uniqueIdentifier, long apartmentId)
-        {
-            Resident newResident = new Resident()
-            {
-                ApartmentId = apartmentId,
-                UniqueIdentifier = uniqueIdentifier
-            };
-
-            context.Residents.Add(newResident);
-            context.SaveChanges();
-        }
-
-        public void UpdateResident(string uniqueIdentifier, string deviceIdentifier, bool alertOnNewParcel)
-        {
-            var existingResident = context.Residents.Where(r => r.UniqueIdentifier == uniqueIdentifier).Single();
-            existingResident.AlertOnNewParcel = alertOnNewParcel;
-            existingResident.DeviceIdentifier = deviceIdentifier;
-            context.SaveChanges();
-        }
-
-        public int GetNumberOfItemsToCollection(string uniqueUserIdentifier)
-        {
-            long apartmentId = GetApartmentIdForResident(uniqueUserIdentifier);
-            int totalPackages = postManager.GetTotalOutstandingPackagesForApartment(apartmentId);
-            return totalPackages;
-        }
-
-        private long GetApartmentIdForResident(string uniqueUserIdentifier)
-        {
-            var apartmentId = context.Residents.Where(r => r.UniqueIdentifier == uniqueUserIdentifier).Select(a => a.ApartmentId).Single();
-            return apartmentId;
-        }
+      context = new PostRoomDataContext();
+      postManager = new PostManager();
     }
+
+    public void AddResident(string uniqueIdentifier, long apartmentId)
+    {
+      var existingResident = context.Residents.Where(r => r.UniqueIdentifier == uniqueIdentifier).SingleOrDefault();
+
+      if (existingResident == null)
+      {
+        Resident newResident = new Resident()
+        {
+          ApartmentId = apartmentId,
+          UniqueIdentifier = uniqueIdentifier,
+          AlertOnNewParcel = true
+        };
+
+        context.Residents.Add(newResident);
+      }
+      else
+      {
+        existingResident.ApartmentId = apartmentId;
+        existingResident.AlertOnNewParcel = true;
+      }
+
+      context.SaveChanges();
+    }
+
+    public void UpdateResident(string uniqueIdentifier, string deviceIdentifier, bool alertOnNewParcel)
+    {
+      var existingResident = context.Residents.Where(r => r.UniqueIdentifier == uniqueIdentifier).Single();
+      existingResident.AlertOnNewParcel = alertOnNewParcel;
+      existingResident.DeviceIdentifier = deviceIdentifier;
+      context.SaveChanges();
+    }
+
+    public int GetNumberOfItemsToCollection(string uniqueUserIdentifier)
+    {
+      long apartmentId = GetApartmentIdForResident(uniqueUserIdentifier);
+      int totalPackages = postManager.GetTotalOutstandingPackagesForApartment(apartmentId);
+      return totalPackages;
+    }
+
+    private long GetApartmentIdForResident(string uniqueUserIdentifier)
+    {
+      var apartmentId = context.Residents.Where(r => r.UniqueIdentifier == uniqueUserIdentifier).Select(a => a.ApartmentId).Single();
+      return apartmentId;
+    }
+  }
 }
